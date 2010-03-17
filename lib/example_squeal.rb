8ad96db244f11c2601c5a98c3cdcceec0f68159f
Josh Graham and Durran Jordan <dev+delitescere+durran@hashrocket.com>
2010-03-17 16:57:17 -0500

    fix example

--- lib/example_squeal.rb
+++ lib/example_squeal.rb
@@ -4,35 +4,25 @@ import('pharmmd_production')
 export('pharmmd_reporting_export')
 
 Squealer.import("patients").find({}).each do |patient|
-  target(:patient, patient.id) do |target|  # insert or update on patient where id is primary key column name
-      target.assign(:name) { patient[first_name] + " " + patient[last_name].upcase }
-      target.assign(:dob) { patient[date_of_birth] }
-      target.assign(:latest_drug) { patient.medications.last.name } # dubious
+  target(:patient, patient.id) do  # insert or update on patient where id is primary key column name
+      assign(:name) { patient[first_name] + " " + patient[last_name].upcase }
+      assign(:dob) { patient[date_of_birth] }
+      assign(:latest_drug) { patient.medications.last.name } # dubious
 
       patient.medications.each do |med|
-
-
-
-
         target(:medication, med.id) do
           assign(:patient_id) { patient.id }
           assign(:name) { med.name }
         end
 
-
-
-
-
-
         med.prescriptions.each do |rx|
-          target(:prescription, rx.id) do |target|
-            target.assign(:patient_id) { patient.id }
-            target.assign(:medication_id) { med.id }
-            target.assign(:dispense_date) { rx.dispense_date }
+          target(:prescription, rx.id) do
+            assign(:patient_id) { patient.id }
+            assign(:medication_id) { med.id }
+            assign(:dispense_date) { rx.dispense_date }
           end
         end
       end
-
     end
   end
 end
@@ -40,12 +30,12 @@ end
 Organization.collection.find({}).each do |organization|
   if organization.disabled
     Patient.collection.find({ :organization_id => organization.id }) do |patient|
-      target(:patient, patient.id) do |target|
-        target.assign(:disabled) { true }
+      target(:patient, patient.id) do
+        assign(:disabled) { true }
       end
     end
 
-    target(:organization, organization.id) do |target|
+    target(:organization, organization.id) do
     end
 
   else
