a1e7b77a483ba826bca41098c741ba1832e7e932
Josh Graham <dev+delitescere@hashrocket.com>
2010-05-16 22:44:12 -0500

    Example changed to show infered row_id

--- lib/example_squeal.rb
+++ lib/example_squeal.rb
@@ -4,18 +4,18 @@ import('localhost', 27017, 'development')
 export('localhost', 'root', '', 'reporting_export')
 
 import.collection("users").find({}).each do |user|
-  target(:user, user) do  # insert or update on user where id is primary key column name
+  target(:user) do  # insert or update on user where id is primary key column name
     assign(:name) { user.first_name + " " + user.last_name.upcase }
     assign(:dob) { user.date_of_birth }
 
     user.activities.each do |activity|
-      target(:activity, activity) do
+      target(:activity) do
         assign(:user_id) {}
         assign(:name) {}
       end
 
       activity.tasks.each do |task|
-        target(:task, task) do
+        target(:task) do
           assign(:user_id) {}
           assign(:activity_id) {}
           assign(:date) {}
@@ -28,7 +28,7 @@ end #collection("users")
 import.collection("organization").find({}).each do |organization|
   if organization.disabled
     import.collection("users").find({ :organization_id => organization.id }) do |user|
-      target(:user, user) do
+      target(:user) do
         assign(:disabled) { true }
       end
     end
